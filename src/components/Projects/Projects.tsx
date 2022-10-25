f0c81c5201202e12bfcf97ca6c530248cfcf79ca
Назар <pagor.n2003@gmail.com>
2022-10-25 16:11:36 +0300

    fix bug

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import style from "./Projects.module.scss";
 import PrimaryButton from "../Buttons/PrimaryButton";
+import {Link} from 'react-router-dom';
 
 
 interface ProjectsProps {
     title: any,
     text: JSX.Element,
     button_text: string,
+    link_button?: string,
 }
 
 
-const Projects = ({text, title, button_text}: ProjectsProps) => {
+const Projects = ({text, title, button_text, link_button}: ProjectsProps) => {
     return (
         <div className={style.project_block}>
             <h1>{title}</h1>
             {text}
-            <PrimaryButton>{button_text}</PrimaryButton>
+            <a href={`${link_button}`} style={{
+                textDecoration:"none"
+            }}>
+                <PrimaryButton>{button_text}</PrimaryButton>
+            </a>
         </div>
     );
 };
